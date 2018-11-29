91298c992e654c9e5707e2ae8a24d0ecf82c0635
Nahid <ashraf.uddin@bs-23.net>
2018-11-29 13:36:22 +0600

    Initial Commit

---
+++ Basic UIView Animation With UICollection View/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Basic UIView Animation With UICollection View
+//
+//  Created by BS126 on 11/29/18.
+//  Copyright © 2018 BS23. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Basic UIView Animation With UICollection ViewTests/Basic_UIView_Animation_With_UICollection_ViewTests.swift
@@ -0,0 +1,34 @@
+//
+//  Basic_UIView_Animation_With_UICollection_ViewTests.swift
+//  Basic UIView Animation With UICollection ViewTests
+//
+//  Created by BS126 on 11/29/18.
+//  Copyright © 2018 BS23. All rights reserved.
+//
+
+import XCTest
+@testable import Basic_UIView_Animation_With_UICollection_View
+
+class Basic_UIView_Animation_With_UICollection_ViewTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
